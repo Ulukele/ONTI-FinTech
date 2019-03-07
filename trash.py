fe8196065506fdd3e92d3a4d399bb8a5ebd1b68d
k05m0navt <gubaydullin.dan@gmail.com>
2019-03-07 15:04:33 +0300

    hello

--- trash.py
+++ trash.py
@@ -15,15 +15,16 @@ with open('faceapi.json') as file:
 cf.BaseUrl.set(BASE_URL)
 cf.Key.set(key)
 
+try:
+    cf.person_group.create(group)
+except:
+    pass
+
 def add_new_person(group, name):
     user_id = cf.person.create(group, name)
     return user_id
 
-def delete_person(group, user_id):
-    cf.person.delete(group, user_id)
-    return user_id
-
-def recognize(fileName, group, name):
+def recognize(fileName, group, user_id):
     vid = str(fileName)
     cap  = cv2.VideoCapture(vid)
     length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -49,31 +50,27 @@ def recognize(fileName, group, name):
             cap.set(2, frame_num)
             cap  = cv2.VideoCapture(vid)
         ret, frame = cap.read()
-        path = 'image.jpg'   
+        path = 'image.jpg'
         cv2.imwrite(path, frame)
         face = cf.face.detect(path)
         if face == []:
             print('Video does not contain any face')
             sys.exit()
         else:
-            try:
-                cf.person_group.create(group)
-            except:
-                pass
-            ID = add_new_person(group, name)
-            persisted_face_id = cf.person.add_face(path, group, ID)
+            persisted_face_id = cf.person.add_face(path, group, user_id)
             face_ids.append(persisted_face_id['persistedFaceId'])
             k += 1
             cap.release()
-    return face_ids, ID
+    return face_ids
 
 args = (sys.argv)[1:]
 datetime_object = datetime.datetime.now()
 name = hash(datetime_object)
 
 if args[0] == '--simple-add':
+    user_id = add_new_person(group, name)
     file_name = args[1]
-    ids, user_id = recognize(file_name, group, name)
+    ids = recognize(file_name, group, user_id['personId'])
     print("5 frames extracted")
     print("PersonId:", user_id['personId'])
     print("FaceIds\n=======")
