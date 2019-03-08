d5e388f0faf27165c1aff6092cafb8fc6e907663
Ulukele <36236949+Ulukele@users.noreply.github.com>
2019-03-09 03:37:03 +0700

    add KYC

--- kyc.py
+++ kyc.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
+import sys
+import json
+from eth_account import Account
+from web3 import Web3, HTTPProvider
+import sha3
 
-### Put your code below this comment ###
-print("Show must go on")
+def GetAdress(privateKey):
+    adress = Account.privateKeyToAccount("0x"+privateKey)
+    return adress
+
+
+
+with open('network.json') as file:
+    infor = json.load(file)
+    privateKey = infor["privKey"]
+    RecURL = infor["rpcUrl"]
+    GasURL = infor["gasPriceUrl"]
+    defGas = infor["defaultGasPrice"]
+
+web3 = Web3(HTTPProvider(RecURL))
+address = GetAdress(privateKey)
